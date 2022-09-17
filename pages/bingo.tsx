c05ef8d27008ce58d0cc4bf98a8ca4aee59bafc1
Einar <einargudnig@gmail.com>
2022-09-17 23:30:18 +0000

    favicon

--- pages/bingo.tsx
+++ pages/bingo.tsx
@@ -114,7 +114,7 @@ const Bingo: NextPage = () => {
       <footer className="flex h-24 w-full items-center justify-center border-t">
         <a
           className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://einargudni.com"
           target="_blank"
           rel="noopener noreferrer"
         >
